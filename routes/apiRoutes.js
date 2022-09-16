c9855b51aa0c28f20278dc6d62274b1e12326be3
femke77 <megan.meyers.388@gmail.com>
2022-09-15 23:11:25 -0700

    replacement function completed and extracted to lib

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,11 @@
 const router = require("express").Router();
-const Post  = require("../models/Post");
-const emoji = require("node-emoji");
+const Post = require("../models/Post");
+const { replaceWordWithEmoji } = require("../lib/emoji");
 
 // router post with emoji converted words
 router.post("/post", async (req, res) => {
-  let body = req.body;
-  // alter the body here
-  body.body += emoji.get(":coffee:") + emoji.get(":heart:")
   try {
-    
+    const body = replaceWordWithEmoji(req.body);
     const newPost = await Post.create(body);
 
     res.json(newPost);
@@ -20,10 +17,9 @@ router.post("/post", async (req, res) => {
 // router get
 router.get("/post", async (req, res) => {
   try {
-   const postData = await Post.findAll({})
-   const posts = postData.map((post) => post.get({ plain: true }));
-   res.json(posts)
-
+    const postData = await Post.findAll({});
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.json(posts);
   } catch (err) {
     res.status(500).json(err.message);
   }
